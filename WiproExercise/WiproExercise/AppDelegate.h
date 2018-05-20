3be727d47eaf5351d41f82982a71d37263e8f3fc
Yogesh141 <yogesh.gupta@zinier.com>
2018-05-20 10:35:25 +0530

    #Added : InitialSetup

---
+++ WiproExercise/WiproExercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WiproExercise
+//
+//  Created by test on 5/20/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WiproExercise/WiproExercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WiproExercise
+//
+//  Created by test on 5/20/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
